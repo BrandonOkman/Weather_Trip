ee78787b023419013f1410e73a6cdec8f8615544
Gatsby <bokmanwd@gmail.com>
2022-02-03 21:40:17 -0700

    fixing html

---
+++ script.js
@@ -0,0 +1,19 @@
+const cityContainer = $('.cities');
+const form = $('#form');
+const submitBtn = $('#submit');
+const cityElement = $('#city');
+const day = $('.day');
+const dayForecast = $('.dayForecast');
+const weekForecast = $('.weekForecast');
+const historyEl = $('#historyItems');
+const div = $('<div class="container">');
+const weeklyForecastDay = $('<div class="weeklyForecastDay">');
+
+// Date
+let d = new Date;
+let month = d.getMonth() + 1;
+let x = d.getDate();
+let year = d.getFullYear();
+let date = '(' + month + '/' + x + '/' + year + ')';
+// History
+let history = getStorage();
