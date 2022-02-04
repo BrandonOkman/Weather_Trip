bc32f7a20867bb86e77fb469b5f1195755c54fab
Gatsby <bokmanwd@gmail.com>
2022-02-03 21:46:37 -0700

    history js

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ let year = d.getFullYear();
 let date = '(' + month + '/' + x + '/' + year + ')';
 // History
 let history = getStorage();
+
+function getStorage() {
+    try {
+        let items = JSON.parse(localStorage.getItem('history'));
+        if (!items) {
+            items = [];
+        }
+        return items;
+    } catch (error) {
+
+    }
+}
+
+historyEl.text(history);
+
+$('.city').click(function(e) {
+    cityElement.val($(e.target).text());
+});
+
+let city;
+const apiKey = '229984962887c500e20428e36f61f8eb';
+
+function makeHistoryButtons() {
+    historyEl.empty();
+    history.forEach((city) => {
+        const cityButton = $('<button class="historyBtn">');
+        cityButton.text(city)
+        historyEl.append(cityButton);
+        // cityButton.dblclick(function() {
+        //     console.log($(this).text());
+        //     let index = history.indexOf($(this));
+        //     console.log(history, 'history b4');
+        //     let newArray = history.splice(index, 1);
+        //     console.log(history, 'history after');
+        //     console.log(newArray, 'new array');
+        //     console.log(index, 'index');
+        //     localStorage.removeItem('history');
+
+        // });
